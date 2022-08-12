82ef98dbbefee8e1c948485deac2c2590029fb24
Umer Kang <umerkang66@gmail.com>
2022-08-12 23:57:35 +0500

    add function package

---
+++ functions/IsPrime.java
@@ -0,0 +1,17 @@
+package functions;
+
+public class IsPrime {
+  public static void main(String[] args) {
+    System.out.println(isPrime(9));
+    System.out.println(isPrime(2));
+  }
+
+  private static boolean isPrime(int n) {
+    for (int i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
--- pattern/BasicsPatterns.java
+++ pattern/BasicsPatterns.java
@@ -1,3 +1,5 @@
+package pattern;
+
 import java.util.Scanner;
 
 public class BasicsPatterns {
