8832ff7362dcfc10dcdc7590b1bdf85d84536616
Umer Kang <umerkang66@gmail.com>
2022-08-13 23:15:51 +0500

    rename the ReverseInteger.java file to NumberComputation.java

--- functions/NumberComputation.java
+++ functions/NumberComputation.java
@@ -1,9 +1,11 @@
 package functions;
 
-public class ReverseInteger {
+public class NumberComputation {
   public static void main(String[] args) {
     int n = 1234;
     System.out.println(reverseInteger(n));
+
+    System.out.println(digitSum(n));
   }
 
   private static int reverseInteger(int n) {
@@ -14,5 +16,15 @@ public class ReverseInteger {
       n /= 10;
     }
     return reverse;
+  }
+
+  private static int digitSum(int num) {
+    int sum = 0;
+    while (num != 0) {
+      int lastDigit = num % 10;
+      sum += lastDigit;
+      num /= 10;
+    }
+    return sum;
   }
 }
