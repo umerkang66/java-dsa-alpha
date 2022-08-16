f82b182b7520401ed7718311f3a59e26cda92872
Umer Kang <umerkang66@gmail.com>
2022-08-16 22:52:29 +0500

    add kadane algorithm

--- arrays/MaxSubArray.java
+++ arrays/MaxSubArray.java
@@ -6,6 +6,7 @@ public class MaxSubArray {
 
     System.out.println(maxSubArrayBF(nums));
     System.out.println(maxSubArrayPrefixArray(nums));
+    System.out.println(subArrayKadaneAlgorithm(nums));
   }
 
   private static int maxSubArrayBF(int[] nums) {
@@ -27,7 +28,6 @@ public class MaxSubArray {
   private static int maxSubArrayPrefixArray(int[] nums) {
     int[] prefixArr = new int[nums.length];
     prefixArr[0] = nums[0];
-
     for (int i = 1; i < nums.length; i++) {
       prefixArr[i] = prefixArr[i - 1] + nums[i];
     }
@@ -40,15 +40,33 @@ public class MaxSubArray {
         int end = j;
         if (i == 0) {
           // if start index 0, end index element should be the total sum
+          // no need to subtract anything
           int subArraySum = prefixArr[end];
           maxSum = Math.max(maxSum, subArraySum);
         } else {
+          // subtract the previous sum from current subArray sum
           int subArraySum = prefixArr[end] - prefixArr[start - 1];
           maxSum = Math.max(maxSum, subArraySum);
         }
       }
     }
 
+    return maxSum;
+  }
+
+  private static int subArrayKadaneAlgorithm(int[] nums) {
+    // if the currentSum is negative, consider it as zero
+    // because if integers that are greater than 0 are present in array
+    // then only that integers in array will return the highest sum
+    int currentSum = 0;
+    int maxSum = Integer.MIN_VALUE;
+    for (int i = 0; i < nums.length; i++) {
+      currentSum += nums[i];
+      if (currentSum < 0) {
+        currentSum = 0;
+      }
+      maxSum = Math.max(maxSum, currentSum);
+    }
     return maxSum;
   }
 }
