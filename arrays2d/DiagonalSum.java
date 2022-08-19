a52488706546dfa74363d4444329ea400ebc724c
Umer Kang <umerkang66@gmail.com>
2022-08-19 12:30:37 +0500

    add diagonal sum

---
+++ arrays2d/DiagonalSum.java
@@ -0,0 +1,67 @@
+package arrays2d;
+
+public class DiagonalSum {
+  public static void main(String[] args) {
+    int[][] matrix = {
+        { 1, 2, 3, 4 },
+        { 5, 6, 7, 8 },
+        { 9, 10, 11, 12 },
+        { 13, 14, 15, 16 }
+    };
+    int[][] matrix2 = {
+        { 1, 2, 3 },
+        { 4, 5, 6 },
+        { 7, 8, 9 }
+    };
+    System.out.println(diagonalSum(matrix));
+    System.out.println(diagSum2(matrix));
+    System.out.println(diagonalSum(matrix2));
+    System.out.println(diagSum2(matrix2));
+  }
+
+  private static int diagonalSum(int[][] matrix) {
+    // matrix always be nxn row==col
+    int row = 0;
+    int col = 0;
+    int sum = 0;
+    // primary diagonal
+    // we can also take col<=matrix[0].length-1;
+    while (row <= matrix.length - 1) {
+      sum += matrix[row][col];
+      row++;
+      col++;
+    }
+    // secondary diagonal
+    row = matrix.length - 1;
+    col = 0;
+    while (row >= 0) {
+      if (row == col) {
+        // when "n" is odd number don't include the single element (center element) that
+        // has already being summed
+        row--;
+        col++;
+        continue;
+      }
+      sum += matrix[row][col];
+      row--;
+      col++;
+    }
+    return sum;
+  }
+
+  private static int diagSum2(int[][] matrix) {
+    int sum = 0;
+    for (int i = 0; i < matrix.length; i++) {
+      // primary diagonal
+      sum += matrix[i][i];
+      // secondary diagonal
+      // i+j = matrix[length]-1 => j = matrix[length]-1-i
+      int j = matrix.length - 1 - i;
+      if (j != i) {
+        // if j==i it is in the primary diagonal
+        sum += matrix[i][j];
+      }
+    }
+    return sum;
+  }
+}
--- arrays2d/SpiralMatrix.java
+++ arrays2d/SpiralMatrix.java
@@ -15,7 +15,6 @@ public class SpiralMatrix {
         { 5, 6, 7, 8 },
         { 9, 10, 11, 12 }
     };
-
     ArrayList<Integer> result = spiralMatrix(matrix);
     System.out.println(result);
     ArrayList<Integer> result2 = spiralMatrix(matrix2);
@@ -23,53 +22,39 @@ public class SpiralMatrix {
   }
 
   private static ArrayList<Integer> spiralMatrix(int[][] matrix) {
-    int startRow = 0;
-    int endRow = matrix.length - 1;
-    int startCol = 0;
-    int endCol = matrix[0].length - 1;
+    int rowStart = 0;
+    int rowEnd = matrix.length - 1;
+    int colStart = 0;
+    int colEnd = matrix[0].length - 1;
+    ArrayList<Integer> result = new ArrayList<>(matrix.length * matrix[0].length);
 
-    ArrayList<Integer> result = new ArrayList<>();
-
-    while (startRow <= endRow && startCol <= endCol) {
-      // print the TOP boundary, where row is fixed (startRow), but columns will
-      // increase, this is loop is for columns
-      for (int j = startCol; j <= endCol; j++) {
-        result.add(matrix[startRow][j]);
+    while (rowStart <= rowEnd && colStart <= colEnd) {
+      // TOP: row is rowStart
+      for (int j = colStart; j <= colEnd; j++) {
+        result.add(matrix[rowStart][j]);
       }
-      // print the RIGHT boundary, where column is fixed (endCol), but row will
-      // increase, this loop is for increasing row
-      for (int i = startRow + 1; i <= endRow; i++) {
-        result.add(matrix[i][endCol]);
+      // RIGHT: col is colEnd
+      for (int i = rowStart + 1; i <= rowEnd; i++) {
+        result.add(matrix[i][colEnd]);
       }
-      // print the BOTTOM boundary, where row is fixed (endRow) but col will decrease
-      // this loop is for decreasing column
-      for (int j = endCol - 1; j >= startCol; j--) {
-        // in odd number of rows and columns
-        if (startRow == endRow) {
-          // if our cols are more, but row is one, break the loop,
-          // otherwise it will be printed twice
+      // BOTTOM: row is rowEnd
+      for (int j = colEnd - 1; j >= colStart; j--) {
+        if (rowStart == rowEnd) {
           break;
         }
-        result.add(matrix[endRow][j]);
+        result.add(matrix[rowEnd][j]);
       }
-      // print the LEFT boundary, where col is fixed(startCol), but row will decrease
-      // this loop is for decreasing row
-      for (int i = endRow - 1; i >= startRow + 1; i--) {
-        // in odd number of rows and columns
-        if (startCol == endCol) {
-          // if our rows are more, but column is one, break the loop
-          // otherwise that element will be printed twice
+      // LEFT: col is colStart
+      for (int i = rowEnd - 1; i >= rowStart + 1; i--) {
+        if (colStart == colEnd) {
           break;
         }
-        result.add(matrix[i][startCol]);
+        result.add(matrix[i][colStart]);
       }
-
-      // When the outer most boundary will be printed, decrease the row and columns
-      // for inner boundaries
-      startRow++;
-      startCol++;
-      endRow--;
-      endCol--;
+      rowStart++;
+      colStart++;
+      rowEnd--;
+      colEnd--;
     }
 
     return result;
