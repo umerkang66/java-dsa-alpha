543f02061d785e973db99778d66b9a0e2a3799a5
Umer Kang <umerkang66@gmail.com>
2022-10-17 20:03:58 +0500

    update tiling problem

--- recursion/recursion_basics/FriendsPairingProblem.java
+++ recursion/recursion_basics/FriendsPairingProblem.java
@@ -9,12 +9,10 @@ public class FriendsPairingProblem {
     if (people == 1 || people == 2) {
       return people;
     }
-
     // people left after single pairing
     int singlePeople = people - 1;
     // people left after double pairing
     int doublePeoples = people - 2;
-
     return friendsPairingProblem(singlePeople) + singlePeople * friendsPairingProblem(doublePeoples);
   }
 }
--- recursion/recursion_basics/TilingProblem.java
+++ recursion/recursion_basics/TilingProblem.java
@@ -1,8 +1,9 @@
 package recursion.recursion_basics;
+// https://www.geeksforgeeks.org/tiling-problem/
 
 public class TilingProblem {
   public static void main(String[] args) {
-    System.out.println(tilingProblem(4));
+    System.out.println(tilingProblem(1)); // 3
   }
 
   // height is always 2 => m = 2
@@ -11,6 +12,10 @@ public class TilingProblem {
     // box (where tiles are placed) height 2, and tile height is also 2.
     // tile width is always 1
     if (n <= 1) {
+      // if all the tiles have been placed. If n has decreased from "n" to 1 (means
+      // only one spot available, and in only 1 way a vertical tile can be placed),
+      // "n" == 0 (means all the spots have been filled), in both cases
+      // one way of putting tiles has been increased
       return 1;
     }
     // if tile is placed in vertical position, width of box should decrease by 1,
