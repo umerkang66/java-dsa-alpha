70951eed7e9f00432f89bc152103dc069f2ade1f
Umer Kang <umerkang66@gmail.com>
2022-12-18 13:10:21 +0500

    add comments in binary string problem

--- recursion/recursion_basics/FriendsPairingProblem.java
+++ recursion/recursion_basics/FriendsPairingProblem.java
@@ -2,25 +2,23 @@ package recursion.recursion_basics;
 
 public class FriendsPairingProblem {
   public static void main(String[] args) {
-    System.out.println(friendsPairingProblem(3));
+    System.out.println(friendsPairing(3));
   }
 
-  private static int friendsPairingProblem(int people) {
-    if (people == 1 || people == 2) {
+  private static int friendsPairing(int n) {
+    if (n == 1 || n == 2) {
       // if there is 1 person, it can only pair be single, here is only 1 possibility
       // so return 1
       // if there is 2 persons, it can remain single, or can pair with other, so here
       // are 2 possibilities, so return 2
-      return people;
+      return n;
     }
 
-    // people left after single pairing
-    int singlePaired = people - 1;
-    // people left after double pairing
-    int doublePaired = people - 2;
+    int singlePaired = friendsPairing(n - 1);
+    int doublePaired = friendsPairing(n - 2);
+    // howManyPeopleRemainToPair * numberOfWays after one pair
+    int pairWays = (n - 1) * doublePaired;
 
-    return friendsPairingProblem(singlePaired) +
-    // pairChoices * numberOfWays after one pair
-        singlePaired * friendsPairingProblem(doublePaired);
+    return singlePaired + pairWays;
   }
 }
--- recursion/recursion_basics/PrintAllBinaryStrings.java
+++ recursion/recursion_basics/PrintAllBinaryStrings.java
@@ -1,19 +1,24 @@
 package recursion.recursion_basics;
 
+// See the video to understand the question
 public class PrintAllBinaryStrings {
   public static void main(String[] args) {
     printAllBinaryStrings(3, 0, "");
   }
 
+  // There shouldn't be consecutive ones
   private static void printAllBinaryStrings(int n, int lastPlace, String str) {
     if (n == 0) {
       System.out.println(str);
       return;
     }
+
     // print 0 in both cases, if lastPlace is 1, or 0
     printAllBinaryStrings(n - 1, 0, str + "0");
+
+    // if lastPlace is 1, don't do anything
     if (lastPlace == 0) {
-      // if print 1, if last place is 0
+      // print 1, if last place is 0
       printAllBinaryStrings(n - 1, 1, str + "1");
     }
   }
