e96403069c60aacd66c4e59e1f6264e8c73aeb8e
Umer Kang <umerkang66@gmail.com>
2022-12-18 11:47:28 +0500

    add comments in friendsPairingProblem

--- recursion/recursion_basics/FriendsPairingProblem.java
+++ recursion/recursion_basics/FriendsPairingProblem.java
@@ -2,17 +2,25 @@ package recursion.recursion_basics;
 
 public class FriendsPairingProblem {
   public static void main(String[] args) {
-    System.out.println(friendsPairingProblem(5));
+    System.out.println(friendsPairingProblem(3));
   }
 
   private static int friendsPairingProblem(int people) {
     if (people == 1 || people == 2) {
+      // if there is 1 person, it can only pair be single, here is only 1 possibility
+      // so return 1
+      // if there is 2 persons, it can remain single, or can pair with other, so here
+      // are 2 possibilities, so return 2
       return people;
     }
+
     // people left after single pairing
-    int singlePeople = people - 1;
+    int singlePaired = people - 1;
     // people left after double pairing
-    int doublePeoples = people - 2;
-    return friendsPairingProblem(singlePeople) + singlePeople * friendsPairingProblem(doublePeoples);
+    int doublePaired = people - 2;
+
+    return friendsPairingProblem(singlePaired) +
+    // pairChoices * numberOfWays after one pair
+        singlePaired * friendsPairingProblem(doublePaired);
   }
 }
--- recursion/recursion_basics/RemoveDuplicatesFromString.java
+++ recursion/recursion_basics/RemoveDuplicatesFromString.java
@@ -6,6 +6,7 @@ public class RemoveDuplicatesFromString {
   public static void main(String[] args) {
     String str = "appnnacollege";
     System.out.println(removeDuplicatesFromString(str, "", 0, new HashSet<Character>()));
+    System.out.println(removeDuplicatesFromString2(str, "", 0, new HashSet<Character>()));
   }
 
   private static String removeDuplicatesFromString(String str, String ans, int index, HashSet<Character> seen) {
@@ -16,6 +17,20 @@ public class RemoveDuplicatesFromString {
       ans += str.charAt(index);
       seen.add(str.charAt(index));
     }
+    return removeDuplicatesFromString(str, ans, index + 1, seen);
+  }
+
+  private static String removeDuplicatesFromString2(String str, String ans, int index, HashSet<Character> seen) {
+    if (index == str.length()) {
+      // If index has pass the last character, return the ans
+      return ans;
+    }
+    if (!seen.contains(str.charAt(index))) {
+      seen.add(str.charAt(index));
+      // If it is not seen, append the character to the ans, and pass that
+      return removeDuplicatesFromString2(str, ans + str.charAt(index), index + 1, seen);
+    }
+    // If it is seen, just add the same answer to the next call
     return removeDuplicatesFromString(str, ans, index + 1, seen);
   }
 }
