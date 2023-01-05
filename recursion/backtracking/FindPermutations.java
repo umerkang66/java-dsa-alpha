bda01c2eac1de4b03c611e576d39b6666f2cb92a
Umer Kang <umerkang66@gmail.com>
2023-01-05 12:13:15 +0500

    update backtracking video

--- recursion/backtracking/FindPermutations.java
+++ recursion/backtracking/FindPermutations.java
@@ -1,14 +1,23 @@
 package recursion.backtracking;
 
+import java.util.HashSet;
+
 public class FindPermutations {
   public static void main(String[] args) {
     String str = "abc";
     findPermutations(str, "");
+
+    System.out.println();
+
+    String strDuplicate = "aba";
+    findPermutationsDuplicate(strDuplicate, "");
   }
 
   private static void findPermutations(String str, String ans) {
     if (str.length() == 0) {
       System.out.println(ans);
+      // Technically we don't need to return it, because string is empty, so the next
+      // for loop will not run
       return;
     }
 
@@ -19,6 +28,30 @@ public class FindPermutations {
       // current characters has been removed from string, pass the string, and add the
       // current char to the ans
       findPermutations(strWithoutCurChar, ans + curChar);
+    }
+  }
+
+  private static void findPermutationsDuplicate(String str, String ans) {
+    // Here "str" is sorted
+
+    if (str.length() == 0) {
+      System.out.println(ans);
+      // Technically we don't need to return it, because string is empty, so the next
+      // for loop will not run
+      return;
+    }
+
+    HashSet<Character> set = new HashSet<>();
+
+    for (int i = 0; i < str.length(); i++) {
+      char curChar = str.charAt(i);
+
+      if (!set.contains(curChar)) {
+        String temp = str.substring(0, i) + str.substring(i + 1);
+        findPermutationsDuplicate(temp, ans + curChar);
+      }
+
+      set.add(curChar);
     }
   }
 }
